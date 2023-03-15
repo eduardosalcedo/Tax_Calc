1d15b4c78c6d40bf912e81d811c692324745e3bf
Eduardo Salcedo <61067530+eduardosalcedo@users.noreply.github.com>
2023-03-14 20:08:41 -0400

    Add files via upload

---
+++ Grade_Calc.py
@@ -0,0 +1,41 @@
+#age = int(input("Enter your age: "))
+#
+#if age >= 67:
+#  print("You can retire!")
+#  
+#print("Hey.")
+
+#pin = input("Enter your PIN: ")
+#
+#if pin == "1234":
+#  print("Access granted! Take your money.")
+#  print("Your balance is low.")
+#else:
+#  print("Incorrect PIN")
+#
+#print("You are done.")
+
+
+grade = float(input("Enter the percentage you got as a grade in your Python course: "))
+
+#if grade >= 90:
+#  print("You get an A! Impressive")
+#elif grade >= 80:
+#  print("You get a B. Well done.")
+#elif grade >= 70:
+#  print("You get a C. Okay, I guess.")
+#elif grade >= 60:
+#  print("You get a D. Not passing this course...")
+#else:
+#  print("You get an F. What a bad grade.")
+###################################################   
+if grade >= 90:
+   print("You get an A! Impressive")
+if grade >= 80:
+      print("You get a B. Well done.")
+if grade >= 70:
+   print("You get a C. Okay, I guess.")
+if grade >= 60:
+   print("You get a D. Not passing this course...")
+else:
+   print("You get an F. What a bad grade.")
